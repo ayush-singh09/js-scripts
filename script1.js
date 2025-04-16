e70c9e7599caf9e44636df14818bdb8b3ecb1405
ayush-singh09 <insaneraj09@gmail.com>
2025-04-16 09:33:57 +0530

    Obfuscated the js code

--- script1.js
+++ script1.js
@@ -1,2 +1,58 @@
-console.log("Hello from Scripts");
-alert("hello from script.")
+function _0x5bf3(_0x180600, _0x39968f) {
+  var _0x2bb5ad = _0x2bb5();
+  return (
+    (_0x5bf3 = function (_0x5bf3ad, _0x302ac9) {
+      _0x5bf3ad = _0x5bf3ad - 0x11d;
+      var _0x4e403f = _0x2bb5ad[_0x5bf3ad];
+      return _0x4e403f;
+    }),
+    _0x5bf3(_0x180600, _0x39968f)
+  );
+}
+var _0x3697b3 = _0x5bf3;
+function _0x2bb5() {
+  var _0x311ee6 = [
+    "9196515WMwYBT",
+    "1580RUkFnT",
+    "35910UiMhcs",
+    "5108ckSYsq",
+    "hello\x20from\x20script.",
+    "3627232HHoZAo",
+    "75674uQAExe",
+    "107129LCwXzC",
+    "3297DuXYsg",
+    "1512nSURcp",
+    "2689030wVbRzq",
+    "log",
+    "29oYSdlH",
+  ];
+  _0x2bb5 = function () {
+    return _0x311ee6;
+  };
+  return _0x2bb5();
+}
+(function (_0x555802, _0x3dd82c) {
+  var _0x3b6323 = _0x5bf3,
+    _0x2cf473 = _0x555802();
+  while (!![]) {
+    try {
+      var _0x15bcf3 =
+        (-parseInt(_0x3b6323(0x129)) / 0x1) *
+          (-parseInt(_0x3b6323(0x123)) / 0x2) +
+        (parseInt(_0x3b6323(0x125)) / 0x3) *
+          (parseInt(_0x3b6323(0x120)) / 0x4) +
+        -parseInt(_0x3b6323(0x127)) / 0x5 +
+        (-parseInt(_0x3b6323(0x11f)) / 0x6) *
+          (parseInt(_0x3b6323(0x126)) / 0x7) +
+        -parseInt(_0x3b6323(0x122)) / 0x8 +
+        -parseInt(_0x3b6323(0x11d)) / 0x9 +
+        (parseInt(_0x3b6323(0x11e)) / 0xa) * (parseInt(_0x3b6323(0x124)) / 0xb);
+      if (_0x15bcf3 === _0x3dd82c) break;
+      else _0x2cf473["push"](_0x2cf473["shift"]());
+    } catch (_0x1f25dd) {
+      _0x2cf473["push"](_0x2cf473["shift"]());
+    }
+  }
+})(_0x2bb5, 0xb31d5),
+  console[_0x3697b3(0x128)]("Hello\x20from\x20Scripts"),
+  alert(_0x3697b3(0x121));
