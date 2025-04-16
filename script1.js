19b9a453e53a36dd0d9088ebc5bf439b6d56bf83
ayush-singh09 <insaneraj09@gmail.com>
2025-04-16 10:38:04 +0530

    added gemini

--- script1.js
+++ script1.js
@@ -1,9 +1,19 @@
 console.log("Hello from Scripts");
-alert("hello from script.")
+alert("hello from script.");
 
-const fun = (API) => {
-    console.log(API);
+const fun = () => {
+  const genAI = new window.GoogleGenerativeAI(API);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const run = async () => {
+    const prompt = "Who is the best chess player";
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    };
     
-}
+    run();
+};
 
 window.fun = fun;
